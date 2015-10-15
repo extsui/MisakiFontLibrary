877c0bdfd2bdb89cf19fb974d7b6efc430085232
extsui <en422641@s.okayama-u.ac.jp>
2015-10-16 00:59:28 +0900

    SJISのデコード機能を追加

--- fntview.py
+++ fntview.py
@@ -17,26 +17,63 @@ def draw_char(stdscr, font, base_x, base_y, ch_x, ch_y):
         index = x + char_to_index(font, ch_x, ch_y)
         for y in range(CH_HEIGHT):
             if font[index] & (1<<y):
-                str = 'X'
+                str = 'XX'
             else:
-                str = ' '
-            stdscr.addstr(base_y + y, base_x + x, str)
+                str = '  '
+            stdscr.addstr(base_y + y, base_x + x*2, str)
+            """
+            # curses.A_REVERSEで反転表示可能．
+            # 'XX'とどっちが見やすいかについては今後検討．
+            if font[index] & (1<<y):
+                stdscr.addstr(base_y + y, base_x + x*2, '  ', curses.A_REVERSE)
+            else:
+                stdscr.addstr(base_y + y, base_x + x*2, '  ')
+            """
 
 def curses_main(stdscr):
     font = np.fromfile('./font/misaki_gothic.fnt', dtype=np.uint8)
 
-    # Test
-    draw_char(stdscr, font, 0+8*0, 0, 1, 3)
-    draw_char(stdscr, font, 0+8*1, 0, 2, 3)
-    draw_char(stdscr, font, 0+8*2, 0, 3, 3)
-    draw_char(stdscr, font, 0+8*3, 0, 4, 3)
-    draw_char(stdscr, font, 0+8*4, 0, 5, 3)
-    draw_char(stdscr, font, 0+8*5, 0, 6, 3)
-    draw_char(stdscr, font, 0+8*6, 0, 7, 3)
-    draw_char(stdscr, font, 0+8*7, 0, 8, 3)
+    # '　' 0x8140 上群左上
+    # '滌' 0x9ffc 上群右下
+    # '漾' 0xe040 下群左上
+    # '熙' 0xeaa4 下群表示可能最終文字
+    ch_array = np.fromstring(u'　滌漾熙'.encode('shift-jis'), dtype=np.uint8)
+    for i in range(0, len(ch_array), 2):
+        hi = ch_array[i]
+        lo = ch_array[i+1]
+        ch_x, ch_y = sjis_to_font(hi, lo)
+        draw_char(stdscr, font, i*8, 0, ch_x, ch_y)
 
     stdscr.getch()
     stdscr.refresh()
+
+def sjis_to_font(hi, lo):
+    """
+    第一バイト
+    ・[0x81～0x9f]と[0xe0～0xef]の範囲に大別される．
+    ・前半であれば，0x81を引くだけでよい．
+    ・後半であれば，0x81と2範囲の空間分を引く．
+    ・" * 2" は第一バイトに2つの区(01～63)が含まれて
+    　いることに対応させる計算である．
+    """
+    if 0x81 <= hi and hi <= 0x9f:
+        ch_y = (hi - 0x81) * 2
+    elif 0xe0 <= hi and hi <= 0xef:
+        ch_y = (hi - 0x81 - (0xe0 - 0x9f - 1)) * 2
+
+    """
+    第二バイト
+    ・[0x40～0x7e]と[0x80～0xfc]の範囲からなる．
+    　見方を変えれば，0x7fのみが抜けた連番である．
+    ・前半であれば，0x40を引くだけでよい．
+    ・後半であれば，0x40と1(0x7fの分)を引く．
+    """
+    if 0x40 <= lo and lo <= 0x7e:
+        ch_x = lo - 0x40
+    elif 0x80 <= lo and lo <= 0xfc:
+        ch_x = lo - 0x40 - 1
+
+    return ch_x, ch_y
 
 if __name__ == '__main__':
     curses.wrapper(curses_main)
